3aeca5f0925ad31c5993d62205f0cdc4fa8e6602
Michael Winton <wintonmr@gmail.com>
2015-04-21 14:30:33 +1200

    Asset pipeline overhaul. application split into backend and frontend. layouts organised and appropriately inheriting for Dryness. sass adapted so it links correctly to the image and font assets.

---
+++ app/assets/javascripts/frontend/pages/home.js
@@ -0,0 +1,52 @@
+/*
+ *  Document   : home.js
+ *  Author     : Xamarin
+ *  Description: Cool word switcher
+ *
+ *= require_self
+ */
+$(function() {
+
+    // Hero word switcher
+
+    console.log("test");
+
+    var switcher = $('#hero-word-switcher');
+    var delay = 3141;
+    var count = switcher.find('span').length;
+
+    function calculateWidths() {
+        switcher.find('span').each(function(index) {
+            $(this).attr('data-width', $(this).width());
+        });
+        switcher.width(switcher.find('.active').attr('data-width'));
+    }
+    calculateWidths();
+
+    $(window).resize(function() {
+        calculateWidths();
+    });
+
+    function doChange() {
+        var nextItem;
+        var currentItem = parseInt(switcher.find('.active').attr('data-oid'));
+
+        if (currentItem == count - 1) {
+            nextItem = 0;
+        } else {
+            nextItem = currentItem + 1;
+        }
+
+        switcher.addClass('in');
+
+        //removes active, adds inactive
+        switcher.find('[data-oid="' + currentItem + '"]').removeClass('active').addClass('inactive');
+        switcher.find('[data-oid="' + nextItem + '"]').addClass('active').removeClass('inactive');
+
+        switcher.width(switcher.find('[data-oid="' + nextItem + '"]').attr('data-width'));
+        setTimeout(doChange, delay);
+    }
+
+    setTimeout(doChange, delay);
+
+});
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,7 +5,22 @@ Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
 Rails.application.config.assets.paths << "#{Rails.root}/vendor/assets/images"
+Rails.application.config.assets.paths << "#{Rails.root}/vendor/assets/fonts"
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+
+#stylesheets
 Rails.application.config.assets.precompile += %w( vendor.css )
+Rails.application.config.assets.precompile += %w( frontend.css )
+Rails.application.config.assets.precompile += %w( backend.css )
+
+#javascripts
+Rails.application.config.assets.precompile += %w( head.js )
+Rails.application.config.assets.precompile += %w( vendor.js )
+
+Rails.application.config.assets.precompile += %w( frontend/frontend_application.js )
+Rails.application.config.assets.precompile += %w( frontend/pages/home.js )
+
+Rails.application.config.assets.precompile += %w( backend/backend_application.js )
+Rails.application.config.assets.precompile += %w( frontend/pages/login.js )
