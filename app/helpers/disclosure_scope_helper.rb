d2dd5d92b85491c091f443c19889f6464775538b
Michael Winton <wintonmr@gmail.com>
2015-06-21 00:21:59 +1200

    made the scopes helper more versatile so that we may exclude specific scopes. e.g. when creating a pitch card, the content scope options should not include "initiator and me".

--- app/helpers/disclosure_scope_helper.rb
+++ app/helpers/disclosure_scope_helper.rb
@@ -1,9 +1,33 @@
 module DisclosureScopeHelper
 
-  def scopes(user)
+  def scopes(user, options={})
+    # Options pattern
+    scope_ids = ["public", "members", "contributors", "initiator", "private"]
+    default_options = {
+        :only => nil,
+        :except => []
+    }
+    options = options.reverse_merge(default_options)
+    options[:only] ||= scope_ids - options[:except]
 
     # Get the current user if signed in, else get a guest user
     curr_user = user != nil ? user : User.new
+
+    scopes = scope_hashes(options[:only])
+
+    # Get the user's defined groups, Members or Geographic
+    curr_user.groups.each do |group|
+      # TODO check that id returns the ID, it may be "_id"
+      group_disclosure_scope = GroupDisclosureScope.new
+      group_disclosure_scope.group = group
+      scopes << { :id => group.id, :scope => group_disclosure_scope,  :name => "my group: "+group.name }
+    end
+
+    scopes
+
+  end
+
+  def scope_hashes(scopes_to_include = nil)
     scopes = []
 
     public = PublicDisclosureScope.new
@@ -19,15 +43,14 @@ module DisclosureScopeHelper
     scopes << { :id => "initiator",   :scope => initiator,      :name => "initiator and me" }
     scopes << { :id => "private",     :scope => private,        :name => "just me" }
 
-    # Get the user's defined groups, Members or Geographic
-    curr_user.groups.each do |group|
-      # TODO check that id returns the ID, it may be "_id"
-      group_disclosure_scope = GroupDisclosureScope.new
-      group_disclosure_scope.group = group
-      scopes << { :id => group.id, :scope => group_disclosure_scope,  :name => "my group: "+group.name }
+    if scopes_to_include == nil
+      # return all
+      scopes
+    else
+      scopes.select { |scope|
+        scopes_to_include.include? scope[:id]
+      }
     end
-
-    scopes
 
   end
 
