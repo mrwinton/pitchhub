df7d0e8c2a13e432945a24f219bde467d32d363e
Michael Winton <wintonmr@gmail.com>
2015-07-12 11:44:11 +1200

    Added dashboard and collabs views.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,6 +3,14 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def index
+
+    # Retrieve the Pitch Cards that the current user is permitted to see
+    permitted_cards = PitchCard.all.select{|pitch_card| can? :read_pitch, pitch_card}
+
+    # Sort most recent first
+    @pitch_cards = permitted_cards.sort_by {|obj| -obj.created_at.to_f}
+
+    render 'index'
   end
 
 end
