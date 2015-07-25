d73b07e7720ec967f6086d622a5f55fe7cdb9b75
Michael Winton <wintonmr@gmail.com>
2015-07-25 21:54:34 +1200

    allowing more comments to go through.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,7 +6,7 @@ class Comment
   include InitiatorAcceptableAndScopable
 
   # == Pagination, max per page
-  paginates_per 10
+  paginates_per 20
 
   belongs_to :author, class_name: "User", inverse_of: :comments
   belongs_to :pitch_card, inverse_of: :comments, class_name: "PitchCard"
