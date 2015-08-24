6ba22902ab103b7bdb09fa1e5e12160420873cce
Gregor Neumayr <erdbirne@gmail.com>
2015-08-24 02:40:46 +0000

    historic examples in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,13 @@ max_comments = 10
 $scopes = DisclosureScopeHelper.scopes(nil)
 
 
+$valid=0
+$challid=1
+$enabid=2
+$solvid=3
+$facilid=4
+$survid=5
+
 def new_user(email, first_name, last_name)
   User.create!(:email => email, :password => 'password123', :first_name => first_name, :last_name => last_name, :access_code => 'PITCH2015')
 end
@@ -32,14 +39,14 @@ def new_pcard(user, cdat, val, chall, enab, solv, facil, surv, img)
   pitch_card.inject_scopes($scopes)
 
   value = PitchPoint.new 
-  value.name = pitch_point_hash_array[0][:name]
+  value.name = pitch_point_hash_array[$valid][:name]
   value.selected = true
   value.value = val
   pitch_card.pitch_points << value
   
   if chall
      challenge = PitchPoint.new 
-     challenge.name = pitch_point_hash_array[1][:name]
+     challenge.name = pitch_point_hash_array[$challid][:name]
      challenge.selected = true
   end
   if chall.length > 0
@@ -51,7 +58,7 @@ def new_pcard(user, cdat, val, chall, enab, solv, facil, surv, img)
   
   if enab
      enable = PitchPoint.new 
-     enable.name = pitch_point_hash_array[2][:name]
+     enable.name = pitch_point_hash_array[$enabid][:name]
      enable.selected = true
   end
   if enab.length > 0
@@ -63,7 +70,7 @@ def new_pcard(user, cdat, val, chall, enab, solv, facil, surv, img)
 
   if solv
      solve = PitchPoint.new 
-     solve.name = pitch_point_hash_array[3][:name]
+     solve.name = pitch_point_hash_array[$solvid][:name]
      solve.selected = true
   end
   if solv.length > 0
@@ -75,7 +82,7 @@ def new_pcard(user, cdat, val, chall, enab, solv, facil, surv, img)
 
   if facil
      facilitate = PitchPoint.new 
-     facilitate.name = pitch_point_hash_array[4][:name]
+     facilitate.name = pitch_point_hash_array[$facilid][:name]
      facilitate.selected = true
      facilitate.value = fac
      pitch_card.pitch_points << facilitate
@@ -83,7 +90,7 @@ def new_pcard(user, cdat, val, chall, enab, solv, facil, surv, img)
 
   if surv
      survey = PitchPoint.new 
-     survey.name = pitch_point_hash_array[5][:name]
+     survey.name = pitch_point_hash_array[$survid][:name]
      survey.selected = true
      survey.value = surv
      pitch_card.pitch_points << survey
@@ -94,9 +101,11 @@ def new_pcard(user, cdat, val, chall, enab, solv, facil, surv, img)
 end
 
 
-def  new_suggestion(pc,pp,u,comment, sugg, status)
+def  new_suggestion(pc,pp,u,cdat,comment, sugg, status, icscope)
   pitch_point = pc.pitch_points[pp]
   suggestion = Suggestion.new
+
+  suggestion.created_at= cdat
 
   suggestion.author = u
   suggestion.pitch_card = pc
@@ -110,19 +119,29 @@ def  new_suggestion(pc,pp,u,comment, sugg, status)
 
   suggestion.i_scope = "public"
   suggestion.c_scope = "public"
-  suggestion.ic_scope = "public"
-  suggestion.status = status
+  suggestion.ic_scope = icscope
+
+  if status == :accepted
+      suggestion.status = :accepted
+
+      pc.pitch_points_attributes = [
+          { id: pitch_point._id, value: sugg }
+      ]
+     
+ 
+  elsif status == :rejected
+      suggestion.status = :rejected
+  end
 
   suggestion.inject_scopes($scopes)
 
   suggestion.save
   pc.comments << suggestion
+  pc.save
 end
 
 
-
 if Rails.env.test?
-
   puts "Environment is TEST"
 
   puts "Cleaning the database"
@@ -233,7 +252,6 @@ if Rails.env.test?
         end
 
         pitch_card.comments << comment
-
       end
 
     end
@@ -303,9 +321,25 @@ else
      'develop a principle for an electrical light',
      '','discharge a battery via carbon electrodes to create an arc', nil, nil, 'swan-lamp.jpg')
 
-  new_suggestion(pitch_card,1,user_js,
+  new_suggestion(pitch_card, $challid, user_js, '01-09-1860',
     'I think we need to develop an electrical light bulb that is much more practical and long-lasting than the arc light bulb',
-    'devise a practical, long-lasting electric light', :accepted)
+    'devise a practical, long-lasting electric light', :accepted, 'public')
+
+  new_suggestion(pitch_card, $solvid, user_js, '01-09-1875',
+    'After some testing, I will suggest to use carbonized paper filaments. They gave me the best results so far. Also, If we enclose it in a vacuum glass bulb, there is less oxygen available and the filament lasts much longer.',
+    'use carbonized paper filament in a vacuum bulb', :accepted, 'public')
+
+  new_suggestion(pitch_card, $challid, user_te, '01-08-1878', 
+    'The stuff that Swan did is quite nice, but I suggest we put down some real money and develop a complete integrated system. Whatever comes out of that, I will certainly claim all credit for myself. At least in the US of America',
+    'develop an entire, integrated system of electric lighting', :accepted, 'members')
+ 
+  new_suggestion(pitch_card, $enabid, user_js, '01-09-1878',
+    'I got the resources to do a rigorous screening to identify the best filament material. Also, I can afford the vacuum pump that Hermann Sprengel invented in 1865.',
+    'the first industrial research ogranisation in Manlo Park can perform screening for best material',:accepted, 'members')
+
+  new_suggestion(pitch_card, $solvid, user_ke, '27-12-1903', 'I like oldscool. Use candles instead of burning coal to generate electricity to produce light and heat', 'use a candle', :rejected, 'initiator') 
+
+  new_suggestion(pitch_card, $solvid, user_te, '27-12-1889', 'We performed a screening and now we know it: Bamboo is hat we want. I like oldscool.', 'bamboo filament in a vacuum bulb', :accepted, 'members') 
 
   pitch_card.save
 
@@ -314,16 +348,66 @@ else
 
   user_as = new_user('as@test.com','Archimedes','of Syracuse')
   user_bh = new_user('bh@test.com','Bill','Hamilton')
-  user_ae = new_user('ae@test.com','an', 'employee')
+  user_af = new_user('af@test.com','Arnold','Franze')
+  user_ae = new_user('ae@test.com','anonymous', 'Employee')
+  user_gd = new_user('gd@test.com','George', 'Davison')
   user_om = new_user('om@test.com','Otis "Dock"','Marston')
 
+  date = Date.today - 2227.years
+  pitch_card = new_pcard(user_as, date, 'transport material against gravity',
+     '','','use a screw to translate rotary motion into linear motion for transportation',
+      nil, 'is this useful to you?', 'archimedes-screw.jpg')
+
+  pitch_card = new_pcard(user_bh, '05-03-1953', 'fast travel on shallow water',
+     'design a hull suitable for a river craft and a replacement for the propeller',
+     'funds and workers available','',
+      nil, nil, 'hamilton-jet.jpg')
+  
+  new_suggestion(pitch_card, $solvid, user_af, '06-04-1953', 
+     'Hey Bill, I suggest you look into jet engines like the one Hanley designed. I cal loan you a US Pamphlet with details.', 
+    'use Hanley Jet', :accepted, 'members') 
+
+  new_suggestion(pitch_card, $solvid, user_ae, '06-04-1955', 
+    'We should eliminate all parts of the design that protrude below the hull exposed to damage. I got the idea from a high pressure firehose..',
+    'integrate jet into the hull', :accepted, 'members') 
+
+  new_suggestion(pitch_card, $solvid, user_gd, '06-07-1956', 
+    'this can be a breakthrough',
+    'use two stage axial flow unit', :accepted, 'members') 
+
+  new_suggestion(pitch_card, $challid, user_om, '04-06-1960', 
+    'I can offer to do the planning and logistics for this. We are going to need 2500 tons of fuel',
+    'go up the grand canyon in three jet boats as a demo',
+    :accepted, 'members')
+
+  pitch_card.save
 
   puts "----------------------------------------------------------------"
 
   user_ah = new_user('ah@test.com','August Wilhelm','von Hofmann') 
   user_wp = new_user('wp@test.com','William Henry','Perkin') 
 
+  pitch_card = new_pcard(user_as, date, 'A cure for Malaria',
+     'find a way to synthesize Quinine','I offer a study place at the Royal College of Chemistry in London','',
+      nil, nil, 'malaria.jpg')
+
+  new_suggestion(pitch_card, $challid, user_wp, '04-06-1960', 
+    'I can offer to do the planning and logistics for this. We are going to need 2500 tons of fuel',
+    'go up the grand canyon in three jet boats as a demo',
+    :accepted, 'members')
+  
+
   puts "----------------------------------------------------------------"
+
+  pitch_card = new_pcard(user_as, date, 'transport material against gravity',
+     '','','use a screw to translate rotary motion into linear motion for transportation',
+      nil, 'is this useful to you?', 'ford-t.jpg')
+
+  pitch_card = new_pcard(user_as, date, 'transport material against gravity',
+     '','','use a screw to translate rotary motion into linear motion for transportation',
+      nil, 'is this useful to you?', 'wheel.jpg')
+
+
   puts "Seeding Pitch Cards"
 
 end
