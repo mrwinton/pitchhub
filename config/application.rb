0583d0aca694c56924813e8e09550f12fb6cca6c
Michael Winton <wintonmr@gmail.com>
2015-09-07 12:20:42 +1200

    Changed to use local_env.yml to manage secrets.

--- config/application.rb
+++ config/application.rb
@@ -37,5 +37,13 @@ module Pitchhub
     config.autoload_paths << "#{Rails.root}/app/validators"
 
     config.web_console.whitelisted_ips = '10.0.2.2'
+
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
+
   end
 end
