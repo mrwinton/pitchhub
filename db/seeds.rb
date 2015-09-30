450811c9014b69036119a3bf21ad99e33bc13972
Michael Winton <wintonmr@gmail.com>
2015-09-30 22:17:27 +1300

    Updated seeds.rb for performance tests.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,10 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
-require 'helper/seeds_helper'
+require 'seeds_helper'
+puts "HEY"
 
-number_of_users = 450000
+number_of_users = 10
 
 def power_law(min,max,n)
   max += 1
@@ -30,7 +31,8 @@ if Rails.env.development?
 
   number_of_users.times do |n|
     SeedsHelper.new_user(Faker::Internet.email, Faker::Name.first_name, Faker::Name.last_name)
-    puts "Seeded user[" + n + "]"
+    n = n+1
+    puts "Seeded user[" + n.to_s + "]"
   end
 
   puts number_of_users.to_s + " users seeded"
@@ -40,26 +42,27 @@ if Rails.env.development?
   index = 0
 
   User.each do |user|
-    puts "User[" +index+ "] seeding"
+    puts "User[" + index.to_s + "] seeding"
     number_of_pitch_cards = generate_power_law_number
-    puts "-- User[" + user._id + "] contributing " + number_of_pitch_cards + " Pitch Cards"
+    puts "-- User[" + user._id + "] contributing " + number_of_pitch_cards.to_s + " Pitch Cards"
 
     number_of_pitch_cards.times do |n|
 
       value_proposition_length = PitchPointsHelper.pitch_point_max_length - 1
       value_proposition = Faker::Lorem.characters(rand(value_proposition_length) + 1)
 
-      new_pcard(user, Faker::Date.between(2.years.ago, Date.today), value_proposition,
+      SeedsHelper.new_pcard(user, Faker::Date.between(2.years.ago, Date.today), value_proposition,
                 SeedsHelper.pitch_point_or_nil,SeedsHelper.pitch_point_or_nil, SeedsHelper.pitch_point_or_nil,
                 SeedsHelper.pitch_point_or_nil, SeedsHelper.pitch_point_or_nil, nil)
 
-      puts "-- -- User[" + user._id + "] contributed " + n + " Pitch Card"
+      puts "-- -- User[" + user._id + "] contributed " + n.to_s + " Pitch Card"
     end
-    puts "User[" +index+ "] seeded"
+    index = index + 1
+    puts "User[" +index.to_s+ "] seeded"
     puts " "
   end
 
 else
-  puts "Not in development: " + Rails.env.to_s
+  puts "Not in development"
 end
 
